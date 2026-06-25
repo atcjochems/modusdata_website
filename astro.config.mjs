193bd6da9cdb0a9342f0dcbd25bab962892bac0a
Arthur <arthur.jochems@gmail.com>
2026-06-25 19:47:26 +0200

    css fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://atcjochems.github.io/modusdata_website',
-  base: '/modusdata_website/',
+  site: 'https://modusdata.ch',
+  base: '/',
 });
